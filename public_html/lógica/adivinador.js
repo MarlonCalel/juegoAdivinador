4499b0f2716ae6daa4d021efdae98f632627081f
marjc <mcalel@bytesw.com>
2024-04-18 21:18:18 -0600

    actualizando

--- public_html/lógica/adivinador.js
+++ public_html/lógica/adivinador.js
@@ -2,5 +2,41 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/ClientSide/javascript.js to edit this template
  */
+export const MAX_INTENTOS_FACIL= 20;
+export const MAX_INTENTOS_DIFICIL= 10;
+export const NUM_MAXIMO_FACIL= 100;
+export const NUM_MAXIMO_DIFICIL= 1000;
+export const EstadoJuego={
+    JUGANDO: "Jugando",
+    GANO: "Gano",
+    PERDIO:"Perdio"
+};
 
+export const ModoJuego={
+    ADIVINADOR: "Adivinador",
+    PENSADOR: "Pensador"
+};
+export const DificultadJuego={
+    FACIL="Facil",
+    DIFICIL="Pensador"
+};
 
+export class Adivinador{
+    #numeroSecreto;
+    #intentoActual;
+    #maxIntentos;
+    #estado;
+    #modo;
+    
+    constructor(dificultad, modo){
+        this.#dificultad=dificultad;
+        this.#modo=modo;
+        this.#intentoActual=0;
+        this.#estado= EstadoJuego.JUGANDO;
+        
+        if(dificultad==DificultadJuego.FACIL){
+           
+        }//Linda Mishell
+    }
+    
+}
